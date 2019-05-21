c92d945218143c18d4443daef7d9e04ba92c0a09
Alcides <aaugusto.kd@gmail.com>
2019-05-21 16:42:23 -0300

    Correcting the matrixElementsSum file

--- matrixElementsSum/index.js
+++ matrixElementsSum/index.js
@@ -1,51 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+function matrixElementsSum(matrix) {
 
-    <script>
-        matriz = [[0,1,1,2], [0,5,0,0], [2,0,3,3]];
-        // matriz = [[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]];
-        
-        function matrixElementsSum(matrix) {
-            console.log(matrix);
-             
-            var elementsSum = 0;
-            var zeroIndexes = [];
+    var elementsSum = 0;
+    var zeroIndexes = [];
 
-            for(i = 0; i < matrix.length; i++){
-                for(j = 0; j < matrix[0].length; j++){
-                    console.log(matrix[i][j]);
+    for(i = 0; i < matrix.length; i++){
+        for(j = 0; j < matrix[0].length; j++){
 
-                    if(matrix[i][j] == 0){
-                        zeroIndexes.push(j);
-                    }
-
-                    if(zeroIndexes.indexOf(j) > -1){
-                        elementsSum += 0;
-                    } else{
-                        elementsSum += matrix[i][j];
-                    }
-
-                }
+            if(matrix[i][j] == 0){
+                zeroIndexes.push(j);
             }
 
-            console.log('elementsSum');
-            console.log(elementsSum);
-            console.log('zeroIndexes');
-            console.log(zeroIndexes);
+            if(zeroIndexes.indexOf(j) > -1){
+                elementsSum += 0;
+            } else{
+                elementsSum += matrix[i][j];
+            }
 
         }
-        
+    }
 
+    return elementsSum;
 
-        matrixElementsSum(matriz)
+}
 
-    </script>
-</body>
-</html>
+matrixElementsSum([[0,1,1,2], [0,5,0,0], [2,0,3,3]]);
