3369d9c1b58e544b6b0d1c5ef5d53ad1bcebf1cd
Alcides A <aaugusto.kd@gmail.com>
2021-06-09 22:38:50 -0300

    add palingromeRearranging algorithm

---
+++ codesignal/palindromeRearranging/index.js
@@ -0,0 +1,11 @@
+function palindromeRearranging(inputString) {
+  return (
+    inputString
+      .split("")
+      .sort()
+      .join()
+      .replace(/(\w)\1/g, "").length < 2
+  );
+}
+
+palindromeRearranging("aab");
